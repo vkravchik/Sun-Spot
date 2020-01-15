c41f536df96b70ffbe55ead4441103b25dc9560c
Vadym <vkravchik@gmail.com>
2020-01-15 22:40:42 +0200

    fix dependency

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-// const express = require('express');
-import express from 'express';
+const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
