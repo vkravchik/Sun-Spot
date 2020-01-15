6df69d8e5ac666d9db821c3c7f734677071a36bb
Vadym <vkravchik@gmail.com>
2020-01-15 20:43:07 +0200

    prepare for deploy

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 app.get('/api/ping', (req, res) => {
     res.send('Pong');
 });
 
-app.listen(process.env.SERVER_PORT || 3001, () =>{
-    console.log('Express server is running on localhost:' + process.env.SERVER_PORT)
+const port = process.env.SERVER_PORT || 3001;
+
+app.listen(port, () =>{
+    console.log('Express server is running on localhost:' + port)
 });
