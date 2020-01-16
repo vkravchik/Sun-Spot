08bf8bd7fb4837a659941724b8f93d4336df34fe
Vadym <vkravchik@gmail.com>
2020-01-16 15:38:19 +0200

    add routes (#1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import User from "./containers/User";
 import Chart from "./containers/Chart";
 
@@ -9,6 +9,18 @@ function App() {
   return (
     <Fragment>
       <Router>
+        <ul>
+          <li>
+            <Link to='/'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/chart'>
+              HighStock Chart
+            </Link>
+          </li>
+        </ul>
         <Route exact path='/'>
           <User/>
         </Route>
