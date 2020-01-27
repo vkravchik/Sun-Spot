a7dcf544b8d75152152636dd66fee18338acbaae
vkravc <vkravchik@gmail.com>
2020-01-27 15:06:52 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
