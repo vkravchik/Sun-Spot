8a97d9de2455cc73b7eea4f89f778dd3a5f0dadd
vkravc <vkravchik@gmail.com>
2020-01-27 16:45:23 +0200

    delete commented code

--- server/utils/parseCsvToJson.js
+++ server/utils/parseCsvToJson.js
@@ -5,13 +5,6 @@ const _ = require('lodash');
 
 module.exports = {
   parseCsvToJson: () => {
-    // let fileInput = path.join(__dirname, '../dataset', 'sunspot_data.csv');
-    // let fileOutput = path.join(__dirname, '../dataset', 'sunspot_data_min.json');
-    //
-    // let json = csvToJson.fieldDelimiter(',').formatValueByType().getJsonFromCsv(fileInput);
-    // fs.writeFileSync(fileOutput, JSON.stringify(json));
-    //
-    // return fileOutput
   },
 
   remapCurrentJson: () => {
