2f54398c892f21ec5373cc7b4d50a07b5cacf269
vkravc <vkravchik@gmail.com>
2020-02-10 16:32:17 +0200

    change logo

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -17,7 +17,7 @@ const NavMenu = () => {
         <Layout.Header>
           <img className="logo"
                alt="logo"
-               src="./sunlogo.png"/>
+               src="./d2logo.png"/>
           <Menu
             theme="dark"
             mode="horizontal"
