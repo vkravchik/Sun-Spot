e7a29b13eff847e76d3fb8f3c03a80542544bc7b
Vadym <vkravchik@gmail.com>
2020-02-20 11:32:50 +0200

    Redux form (#11)
    * change to redux-from
    
    * add style
    
    * add validate for form
    
    * created style for error
    
    * created rule for spreding Input props

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
+import { reducer as formReducer } from 'redux-form';
 import { pieReducer } from './pieReducer';
 import { highStockReducer } from './highStockReducer';
 import { switchTypeReducer } from './switchTypeReducer';
@@ -17,5 +18,6 @@ export default combineReducers({
   errorHandlerReducer,
   heroesReducer,
   matchesReducer,
-  customChartReducer
+  customChartReducer,
+  form: formReducer
 });
